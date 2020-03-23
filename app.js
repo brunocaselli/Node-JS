0ec8d76dbfc8dfef138882be98e71152fea22ecd
brunocaselli <brunocaselli@icloud.com>
2020-03-23 13:17:46 -0300

    Agrego mi primer archivo

---
+++ app.js
@@ -0,0 +1,7 @@
+    var fruits = ["Banana", "Orange", "Apple", "Mango"];
+    if(Array.isArray(fruits) == true){
+        console.log("True");
+    }else{
+        console.log("False");        
+    }
+    
